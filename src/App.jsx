f631ebb9351000258072330e4bfefa3733e10adf
goforpordev <goforprodev@gmail.com>
2022-12-09 09:28:50 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+function App() {
+  return (
+    <ChakraProvider>
+      <div>
+        
+        </div>;
+    </ChakraProvider>
+  );
+}
+
+export default App;
